310d370267959c18691a8e357c27e466de0befbb
Kiitan Oluwasere <kiitanoluwasere@prevail-partners.com>
2025-05-29 19:07:11 +0100

    feat(phase1): half finished push of sprint 3, will try to finish tonight, if not then - tomo

---
+++ phase1/phase1-sprint3.py
@@ -0,0 +1,29 @@
+from functools import partial
+from collections.abc import Callable
+
+def power_of(x: int, y: int) -> int:
+    return pow(x, y)
+
+square = partial(power_of, y=2)
+
+def my_partial(func: Callable, *preset_args, **preset_kwargs) -> Callable:
+
+    def wrapper(*args, **new_kwargs):
+        seq_tupl = preset_args + args
+        merged_kwargs = {**preset_kwargs, **new_kwargs}
+
+        return func(*seq_tupl, **merged_kwargs)
+
+    return wrapper
+
+my_square = my_partial(pow, exp=2)
+
+
+
+if __name__ == "__main__":
+    assert square(5) == 25, f"sqaure partial defined incorrectly expecting 25, but returned {square(5)} instead"
+
+    assert my_square(7) == square(7), f"expecting {True} because 49 == 49"
+    assert my_square(10) == square(10), f"expecting {True} because 100 == 100"
+
+    
