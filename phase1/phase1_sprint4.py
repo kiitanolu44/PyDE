44f717fd83a60b39fbe5855b5df0573c23fbf4bd
Kiitan Oluwasere <kiitanoluwasere8@gmail.com>
2025-06-18 22:08:32 +0100

    feat(phase1): finally finished with phase 1, phase 2 starts next

--- phase1/phase1_sprint4.py
+++ phase1/phase1_sprint4.py
@@ -1,10 +1,10 @@
 import sys
 from collections.abc import Callable
 import logging
-from logging import Logger
 from functools import wraps
-from phase1_sprint2 import memoize
+
 from functools import lru_cache
+from time import perf_counter_ns
 
 from PyDE_utils import setup_logging
 
@@ -20,7 +20,7 @@ def log_calls(func: Callable) -> None:
     @wraps(func)
     def wrapper(*args, **kwargs):
         logger.info("Calling...")
-        func()
+        return func(*args, **kwargs)
 
     return wrapper
 
@@ -29,6 +29,12 @@ def log_calls(func: Callable) -> None:
 def greet(name):
     """say hello"""
     return f"Hi {name}"
+
+@lru_cache(maxsize=None)
+def fibonacci(n: int) -> int:
+    if n < 2:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 def main() -> None:
     setup_logging()
@@ -51,5 +57,25 @@ def main() -> None:
     assert greet.__name__ == "greet"
     assert greet.__doc__ == "say hello"
 
-if "__main__" == __name__:
+    fib_1_start = perf_counter_ns()
+    fibonacci(30)
+    fib_1_stop = perf_counter_ns()
+
+    fib_1_duration = fib_1_stop - fib_1_start
+
+    fib_2_start = perf_counter_ns()
+    fibonacci(30)
+    fib_2_stop = perf_counter_ns()
+
+    fib_2_duration = fib_2_stop - fib_2_start
+
+    logger.info("fib 1 call is %d nanoseconds, fib 2 call is %d nanoseconds", fib_1_duration, fib_2_duration)
+
+    assert fib_2_duration < 0.1 * fib_1_duration 
+
+
+if __name__ == "__main__":
     sys.exit(main())
+
+# wraps allowed me to access the nested function's information
+# lru cache sped up my fib call by more than 90%
