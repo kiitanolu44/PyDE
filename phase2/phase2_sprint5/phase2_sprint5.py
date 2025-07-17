2e23b881364e67ab2678516482d2714b12cbec6e
Kiitan Oluwasere <kiitanoluwasere@prevail-partners.com>
2025-07-17 10:47:16 +0100

    feat(phase 2): sprint 5 task 1 - 3

---
+++ phase2/phase2_sprint5/phase2_sprint5.py
@@ -0,0 +1,53 @@
+import sys
+import pathlib
+import os
+import importlib.resources
+
+def main() -> None:
+
+    script_path = pathlib.Path(__file__).resolve()
+    sprint4 = script_path.parent.parent/"phase2_sprint4"
+    sys.path.insert(0, str(sprint4))
+    
+    # 1
+
+    text_import = importlib.resources.files("pkg").joinpath("data", "greeting.txt")
+    text = text_import.read_text(encoding="utf-8")
+
+    # assert importlib.resources.files("pkg.data").joinpath("greeting.txt") == "Hello, Data!"
+
+    assert text.strip() == "Hello, Data!"
+
+    # 2
+
+    import pkg
+
+    assert pkg.__version__ == "0.1.0"
+
+    # 3
+
+    os.environ["USE_FAST"] = "0"
+
+    if os.environ.get("USE_FAST") == "1":
+        from fast_impl import work
+    else:
+        from safe_impl import work
+
+    
+    assert work() == "safe"
+
+    os.environ["USE_FAST"] = "1"
+
+    if os.environ.get("USE_FAST") == "1":
+        from fast_impl import work
+    else:
+        from safe_impl import work
+
+    assert work() == "fast"
+
+    # 4
+
+    
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ phase2/phase2_sprint5/safe_impl.py
@@ -0,0 +1,2 @@
+def work():
+    return "safe"
