40177f83e3b24c174d3dc862b32933c28265b9e9
Kiitan Oluwasere <kiitanoluwasere@prevail-partners.com>
2025-07-18 16:21:47 +0100

    feat(phase3): kick off phase 3, sprint 1 task 1 complete

--- phase3/phase3_sprint1/phase3_sprint1.py
+++ phase3/phase3_sprint1/phase3_sprint1.py
@@ -1,25 +1,30 @@
 import sys
 import csv
-from tempfile import NamedTemporaryFile
-import shutil
 
 
 def main() -> None:
-    fields = ["Name", "Age"]
-    tempfile = NamedTemporaryFile(mode="w", delete=False)
 
-    with open("data.csv", mode="r") as f, tempfile:
-        reader = csv.DictReader(f, fieldnames=fields)
-        writer = csv.DictWriter(f, fieldnames=fields)
+    # 1
+
+    with open("data.csv", mode="r") as file, open("out.csv", mode="w") as tempfile:
+        reader = csv.DictReader(file)
+        writer = csv.DictWriter(tempfile, fieldnames=reader.fieldnames)
+        writer.writeheader()
+
+        check = []
 
         for row in reader:
-            age = int(float(row["Age"]))
-            row["Age"] = str(age + 1)
-
-            print(row)
+            row["Age"] = int(row["Age"])
+            row["Age"] = row["Age"] + 1
             writer.writerow(row)
+            check.append(row)
+            # print(row)
+            
+        assert check[0].get("Name") == "Alice" and check[0].get("Age") == 31
+        assert check[1].get("Name") == "Bob" and check[1].get("Age") == 26
+        assert check[2].get("Name") == "Carol" and check[2].get("Age") == 41
 
-    shutil.move(tempfile, f)
+    # 2
 
 
 if __name__ == "__main__":
