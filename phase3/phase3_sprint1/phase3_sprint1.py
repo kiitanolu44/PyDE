5401fe774fa6056b1a0a3a097fc6e1f4cd803ea4
Kiitan Oluwasere <kiitanoluwasere@prevail-partners.com>
2025-07-22 14:01:47 +0100

    feat(phase3): task 2

--- phase3/phase3_sprint1/phase3_sprint1.py
+++ phase3/phase3_sprint1/phase3_sprint1.py
@@ -1,5 +1,8 @@
 import sys
 import csv
+import json
+import struct
+import contextlib
 
 
 def main() -> None:
@@ -25,6 +28,28 @@ def main() -> None:
         assert check[2].get("Name") == "Carol" and check[2].get("Age") == 41
 
     # 2
+
+    with open("base.json", "r") as base_file:
+        base = json.load(base_file)
+
+    with open("override.json", "r") as override_file:
+        override = json.load(override_file)
+
+    merged = base.copy()
+    
+    for key, value in override.items():
+        if key not in merged:
+            merged[key] = value
+        else:
+            merged[key].extend(override["flags"])
+
+    expected_dict = {
+        "threshold": 10,
+        "flags": ["a", "b", "c"],
+        }
+
+    assert merged == expected_dict
+
 
 
 if __name__ == "__main__":
