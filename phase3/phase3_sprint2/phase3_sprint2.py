c6dc9e8f093290a517d2617f063712b262cf48a1
Kiitan Oluwasere <kiitanoluwasere@prevail-partners.com>
2025-07-24 11:05:19 +0100

    feat(phase3): task 2 complete

--- phase3/phase3_sprint2/phase3_sprint2.py
+++ phase3/phase3_sprint2/phase3_sprint2.py
@@ -1,7 +1,6 @@
 import sys
 import yaml
-from pprint import pprint
-
+import xml.etree.ElementTree as ET
 
 
 def main() -> None:
@@ -22,8 +21,34 @@ def main() -> None:
     with open("config.yaml", "r") as file:
         yaml_file = yaml.safe_load(file)
     
-
     assert yaml_file == yaml_check
+
+    # 2
+
+    root = ET.Element("root")
+    user = ET.SubElement(root, "user", id="1")
+    ET.SubElement(user, "name").text = "Alice"
+
+    user = ET.SubElement(root, "user", id="2")
+    ET.SubElement(user, "name").text = "Bob"
+
+    user = ET.SubElement(root, "user", id="3")
+    ET.SubElement(user, "name").text = "Carol"
+
+    # print(user)
+
+    assert root[1][0].text == "Bob"
+
+    user_count = 0
+
+    for user in root:
+        user_count += 1
+
+    assert user_count == 3
+
+    # assert root[1]
+
+
 
 
 if __name__ == "__main__":
